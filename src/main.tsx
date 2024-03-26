b06eed662aab21c8c55baa089cf1ff692caa5d64
Rogério Fernandes <106206470+RogerioFernandesSilva@users.noreply.github.com>
2024-03-26 15:41:47 -0300

    Ajustes, eslintrc, Readme.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { GlobalStyle } from "./styles/global/styles.tsx";
+import { GlobalStyle } from "./styles-global/styles2.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
